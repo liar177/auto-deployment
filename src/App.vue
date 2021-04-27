73d78d1b249cc60af71260342eefd8ce81077267
liar177 <936633856@qq.com>
2021-04-27 15:59:17 +0800

    自动部署第一次测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <div style="font-size:26px">手动部署流程显示</div>
+    <div style="font-size:26px">手动部署流程显示:</div>
+    <div>开发者从本地修改代码->本地git add commit -> 提交到github -> github Action开始自动部署 ->等待十分钟左右就能上线显示 </div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
